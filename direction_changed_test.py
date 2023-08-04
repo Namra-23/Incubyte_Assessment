6ddc31347cfb72c84865eac720f0fbe6288967d1
Namra-23 <namrajoshi2303@gmail.com>
2023-08-04 13:03:57 +0530

    changeDirection_test_leftTorNorth

---
+++ direction_changed_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestChangeDirection(unittest.TestCase):
+
+    # Test cases for rotation in the left direction
+    def test_left_North(self):
+        # Test changing direction when initially facing North and rotating left
+        initial_direction = "N"
+        rotate_to = "L"
+        self.assertEqual(change_direction(initial_direction, rotate_to), "W")
