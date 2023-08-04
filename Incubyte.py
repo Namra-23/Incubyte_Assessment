14cadc5154d2038aa7c17c073074a424db678909
Namra-23 <namrajoshi2303@gmail.com>
2023-08-04 12:08:16 +0530

    backward_test_east_handled

--- Incubyte.py
+++ Incubyte.py
@@ -19,5 +19,7 @@ def move_backward(position, direction):
     new_position = position.copy()
     if direction == "N":
         new_position[1] -= 1
+    elif direction == "E":
+        new_position[0] -= 1
     
     return new_position
