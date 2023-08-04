8a74bbcf1be952294584e9696b271130b9e4083a
Namra-23 <namrajoshi2303@gmail.com>
2023-08-04 11:43:40 +0530

    forward_test_south_handled

--- Incubyte.py
+++ Incubyte.py
@@ -6,5 +6,7 @@ def move_forward(position, direction):
         new_position[0] += 1
     elif direction == "U":
         new_position[2] += 1
+    elif direction == "S":
+        new_position[1] -= 1
     
     return new_position
