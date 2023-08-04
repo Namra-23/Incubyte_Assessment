6af0828f3156058af82eda0d1902ef652af1a198
Namra-23 <namrajoshi2303@gmail.com>
2023-08-04 12:12:48 +0530

    backward_test_west

--- backward_test.py
+++ backward_test.py
@@ -27,5 +27,11 @@ class TestMoveBackward(unittest.TestCase):
         position = move_backward(position, direction)
         self.assertEqual(position, [0, 1, 0])
         
+    def test_West(self):
+        direction = "W"
+        position = [0, 0, 0]
+        position = move_backward(position, direction)
+        self.assertEqual(position, [1, 0, 0])
+        
 if __name__ == '__main__':
     unittest.main()
