f06ae203a28eda9e0caf52b180279ccc0e10bee8
Namra-23 <namrajoshi2303@gmail.com>
2023-08-04 10:47:53 +0530

    direction

---
+++ direction.py
@@ -0,0 +1,29 @@
+direction_for_north = {
+    "L": "W",
+    "R": "E",
+}
+
+direction_for_south = {
+    "L": "E",
+    "R": "W",
+}
+
+direction_for_east = {
+    "L": "N",
+    "R": "S",
+}
+
+direction_for_west = {
+    "L": "S",
+    "R": "N",
+}
+
+direction_for_up = {
+    "L": "W",
+    "R": "E",
+}
+
+direction_for_down = {
+    "L": "E",
+    "R": "W",
+}
