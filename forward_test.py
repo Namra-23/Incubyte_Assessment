4a697867c4c7e13f30f95c33ee6476ef004b2261
Namra-23 <namrajoshi2303@gmail.com>
2023-08-04 11:42:54 +0530

    forward_test_south

--- forward_test.py
+++ forward_test.py
@@ -21,6 +21,12 @@ class TestMoveForward(unittest.TestCase):
         position = move_forward(position, direction)
         self.assertEqual(position, [0, 0, 1])
         
+    def test_South(self):
+        direction = "S"
+        position = [0, 0, 0]
+        position = move_forward(position, direction)
+        self.assertEqual(position, [0, -1, 0])
+        
 
 if __name__ == '__main__':
     unittest.main()
